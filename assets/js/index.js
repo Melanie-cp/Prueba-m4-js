85e217c630d474bdb2cdb444a33d4ba73a44cc1a
Melanie <melanie.collins.p@gmail.com>
2024-03-19 13:06:40 -0300

    procesando el formulario

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,6 +22,7 @@ getData()
 
 const Tabla = document.querySelector("#Tabla")
 const Animales = document.querySelector("#Animales")
+const animalFormulario = document.querySelector("#animalFormulario")
 const animalSeleccion = document.querySelector("#animalSeleccion")
 const edadSeleccion = document.querySelector("#edadSeleccion")
 const comentarios = document.querySelector("#comentarios")
@@ -30,3 +31,8 @@ const btnRegistrar = document.querySelector("#btnRegistrar")
 const audioPlayer = document.querySelector("#audioPlayer")
 const modalAnimal = document.querySelector("#modalAnimal")
 
+
+animalFormulario.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+    console.log("me estas procesando")
+})
